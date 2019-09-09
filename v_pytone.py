5c2884ee57aa5cf4c62f16e88b16d7499540257e
JIuH4 <KJIuH4@gmail.com>
2019-09-09 16:28:15 +0700

    v_pytone class user

--- v_pytone.py
+++ v_pytone.py
@@ -20,7 +20,7 @@ class User:
         if isinstance(new_friend_s, list):
             for user in new_friend_s:
                 if isinstance(user, User):
-                    if self.friends.count(user) == 0 and self!=user:
+                    if self.friends.count(user) == 0 and self != user:
                         self.friends.append(user)
         elif isinstance(new_friend_s, User):
             self.friends.append(new_friend_s)
@@ -30,9 +30,9 @@ user1 = User("Poul", datetime.date(1987, 11, 2))
 user2 = User("Ben", datetime.date(1983, 2, 10))
 user3 = User("Chuck", datetime.date(1980, 1, 20))
 
-user1.add_friends([user1,user2, user3, user3,"DSFA"])
-user2.add_friends([user1,user2, user3, user3,"DSFA"])
-user3.add_friends([user1,user2, user3, user3,"DSFA"])
+user1.add_friends([user1, user2, user3, user3, "DSFA"])
+user2.add_friends([user1, user2, user3, user3, "DSFA"])
+user3.add_friends([user1, user2, user3, user3, "DSFA"])
 
 print(user1.name, user1.get_age(), [u.name for u in user1.friends])
 
